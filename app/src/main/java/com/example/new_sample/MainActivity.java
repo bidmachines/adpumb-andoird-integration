8d97b65e9617f52c429b571f1e0270fe6ed44f21
arshed-gulshan <arshed.gulshan@pegasus.ae>
2021-08-02 01:03:04 +0400

    multidex support

--- app/src/main/java/com/example/new_sample/MainActivity.java
+++ app/src/main/java/com/example/new_sample/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Adpump.register(this,true);
+        Adpump.register(this,BuildConfig.DEBUG);
         setContentView(R.layout.activity_main);
         viewSetup();
 
